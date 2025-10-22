6adeaa625e854e8948bfe116710e2f208eb3b727
reus <carol.devops.25@gmail.com>
2025-10-22 23:01:11 +0700

    log

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express')
-
+const nodemailer = require('nodemailer')
 const app = express()
 require('dotenv').config()
 
 const PORT = process.env.PORT
 
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD, // <-- Your app password here
+  },
+});
+const mailOptions = {
+  from: process.env.EMAIL_PASSWORD,
+  to: process.env.RECEIVER_EMAIL,
+  subject: "Hello from Transcoding Worker!",
+  text: "This email was sent using a Gmail app password. I receive an API call.",
+};
 app.get('/', (req, res) => {
     return res.status(200).json({
         message: "/ route"
@@ -12,6 +26,15 @@ app.get('/', (req, res) => {
 })
 
 app.post('/upload', (req, res) => {
+    console.log('sent from:', mailOptions.from)
+    console.log('sent to:', mailOptions.to)
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
     return res.status(200).json({
         status: "receive message"
     })
