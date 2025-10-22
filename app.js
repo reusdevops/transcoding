83f8fb0fe6fc0f2514460d1ddbc1ada5333659a3
reus <carol.devops.25@gmail.com>
2025-10-22 10:46:57 +0700

    added route

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const app = express()
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+app.get('/', (req, res) => {
+    return res.status(200).json({
+        message: "/ route"
+    })
+})
+
+app.post('/upload', (req, res) => {
+    return res.status(200).json({
+        status: "receive message"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log('server is listening on port :', PORT)
+}
+)
